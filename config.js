471c542de6eba77548a626c7c08073ba0891b60d
dinuboy <143182245+dinuboy@users.noreply.github.com>
2023-08-25 16:32:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94766529671', 'me', true],
+  ['94766529671', 'vidu boy', true], 
   [''] 
 ] //Number of owners
 
